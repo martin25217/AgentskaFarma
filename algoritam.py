9d31db640e0c6ef91cad52aedd89cb52a1e8cbe8
LaraVisnjan <la.uljevic@gmail.com>
2026-08-02 20:06:25 +0200

    Eo nacrta za algoritam

--- algoritam.py
+++ algoritam.py
@@ -1,6 +1,20 @@
-import random*
-populacija
-numeriraj populacija
-bodovanje
-izdvojimo 30%
-dva_broja=random.sample(populacija,k=2)
+
+eval_modeli=[]
+for model in modeli:
+    eval_modeli.append((_,model))
+    a=[2,5,6,8,3]
+
+
+sort=list(reversed(a))
+print(sort)
+b=slice(10)
+print(sort[b])
+
+import random
+agenti=[1,4,6,3,9,8,34,64,654,324,544,]
+p1=random.choice(agenti)
+agenti.pop(agenti.index(p1))
+p2=random.choice(agenti)
+agenti.pop(agenti.index(p2))
+j=[p1,p2]
+print(j,agenti)
---
+++ probno.py
@@ -0,0 +1,5 @@
+a=[2,5,6,8,3]
+sort=list(reversed(a))
+print(sort)
+b=slice(2)
+print(sort[b])
