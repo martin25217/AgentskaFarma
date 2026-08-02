aef854a771c856238b9aaff7dd463ba0b63f8887
vanjaaww <vpetropoli@gmail.com>
2026-08-02 23:15:40 +0200

    fix

--- algoritam.py
+++ algoritam.py
@@ -2,12 +2,16 @@ from gym import Model
 import random 
 import pickle as pc
 
-kol = 10
-dijete = []
+kol = 2
+
 populacija= []
-for i in range (101):
-    populacija.append (Model([10,10,10,10,10,9], 33600))
-sort=list(reversed(populacija))
+for i in range (3):
+    x = Model([kol,kol,kol,kol,kol,9], 33600)
+    r1 = x.eval_model()
+    populacija.append([r1,x])
+
+populacija.sort(key=lambda item: item[0], reverse=True)
+
 b=slice(10)
 populacija = (sort[b])
 for i in range (91):
