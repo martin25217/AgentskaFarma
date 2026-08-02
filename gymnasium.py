a760ff35bbeddbea915cf25cadb3a4f2636678eb
Filip Persuric Bernobic <filipperberp@gmail.com>
2026-08-02 12:53:27 +0200

    feat: random movement and human mode

--- gymnasium.py
+++ gymnasium.py
@@ -7,8 +7,9 @@ import gymnasium as gym
 
 gym.register_envs(ale_py)
 
-env = gym.make("ALE/MsPacman-v5")
+env = gym.make("ALE/MsPacman-v5", render_mode="human")
 obs, info = env.reset()
-obs, reward, terminated, truncated, info = env.step(env.action_space.sample())
-print(obs.shape, reward, terminated, truncated)
+while True:
+    obs, reward, terminated, truncated, info = env.step(env.action_space.sample())
+    print(obs.shape, reward, terminated, truncated)
 env.close()
