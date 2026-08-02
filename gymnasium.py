932a30b3891abc005feba72af95be5e8c2b9b774
Filip Persuric Bernobic <filipperberp@gmail.com>
2026-08-02 16:30:18 +0200

    feat: flatten 2d array to vector

--- gymnasium.py
+++ gymnasium.py
@@ -2,13 +2,13 @@ import sys
 
 sys.path.pop(0)
 
-#import ale_py
+import ale_py
 import gymnasium as gym
 
 gym.register_envs(ale_py)
 
 env = gym.make("ALE/MsPacman-v5", obs_type="grayscale", render_mode="rgb_array")
-
+env = gym.wrappers.FlattenObservation(env)
 
 def model(_obs):
     return env.action_space.sample()
