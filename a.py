e7e5bbed5aa206fdac3c4886d786d2206894a703
krajinakarla-spec <krajinakarla@gmail.com>
2026-08-05 18:24:12 +0200

    hihihi

--- a.py
+++ a.py
@@ -1,45 +1,30 @@
 import gymnasium as gym
 import ale_py
+import pygame
+
+from gymnasium.utils.play import play
+
 
 gym.register_envs(ale_py)
 
-env = gym.make("ALE/MsPacman-v5", render_mode="human")
+env = gym.make(
+    "ALE/MsPacman-v5",
+    render_mode="rgb_array"
+)
 
-import pygame
+print(env.unwrapped.get_action_meanings())
 
-pygame.init()
-pygame.display.set_mode((200, 200))
 
-key_to_action = {
-    pygame.K_UP: 1,
-    pygame.K_RIGHT: 2,
-    pygame.K_LEFT: 3,
-    pygame.K_DOWN: 4,
+keys_to_action = {
+    (pygame.K_UP,): 1,
+    (pygame.K_RIGHT,): 2,
+    (pygame.K_LEFT,): 3,
+    (pygame.K_DOWN,): 4,
 }
 
-obs, info = env.reset()
 
-done = False
-
-action = 0  # NOOP
-
-while not done:
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-
-    keys = pygame.key.get_pressed()
-
-    action = 0  # NOOP by default
-    for key, mapped_action in key_to_action.items():
-        if keys[key]:
-            action = mapped_action
-            break
-
-    obs, reward, terminated, truncated, info = env.step(action)
-
-    done = terminated or truncated
-
-env.close()
-pygame.quit() 
+play(
+    env,
+    keys_to_action=keys_to_action,
+    fps=15
+)
